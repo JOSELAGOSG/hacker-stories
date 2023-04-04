dedee35425688de4d9255150bddb36669ee256ed
José Lagos Guevara <joselagosg@Joses-Mac-mini.local>
2023-04-03 21:52:43 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,65 @@
 import * as React from 'react';
 
-const myObject = [
+const list = [
   {
     title: 'React',
-    url: 'https://reactsjs.org/',
-    number: 1,
-    author: 'Michael Leppe'
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
   },
   {
-    title: 'Angular',
-    url: 'https://angularjs.org/',
-    number: 2,
-    author:'Maicol Jackson'
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
-  {
-    title: 'Vue',
-    url: 'https://vuejs.org/',
-    number: 3,
-    author:'Extra chromosome enjoyer'
-  }
-]
+];
 
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
+    <Search />
 
-function List() {
-  return (
-    <ul>
-      {myObject.map(function (item) {
-        return (
-          <li key={item.number}>
-            <span>
-              <a href={item.url}>Title(with url): {item.title}</a>
-            </span>
-            <br></br>
-            <span>
-              Author: {item.author}
-            </span>
-            <br></br>
-            <span>
-              Key: {item.number}
-            </span>
-            
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+    <hr />
 
+    <List />
+  </div>
+);
 
-
-
-
-function App() {
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of a target (here: input HTML element)
+    console.log(event.target.value);
+  };
   return (
     <div>
-      <h1>Hello World</h1>
       <label htmlFor='search'>Search:</label>
-      <input id='search' type='text'/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <List />
+      <input id='search' type="text" onChange={handleChange}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default App;
